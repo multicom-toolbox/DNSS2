5028462ad89bd0ec023ff55644069497d87c59ed
Jie Hou <jhou4@hou.mcs.slu.edu>
2020-07-08 17:00:49 -0500

    Add virutal environment installation

--- train_DNSS2/models/Deep1Dconv_ss/scripts/runCNNSS_train_3class.sh
+++ train_DNSS2/models/Deep1Dconv_ss/scripts/runCNNSS_train_3class.sh
@@ -7,18 +7,26 @@
 #SBATCH -t 2-00:00
 #SBATCH --mem 20G
 
-source /storage/hpc/scratch/zggc9/keras_theano/keras_virtual_env/bin/activate
-echo "Training secondary structure"
-module load R/R-3.3.1
-
 export HDF5_USE_FILE_LOCKING=FALSE
 temp_dir=$(pwd)
 GLOBAL_PATH=${temp_dir%%DNSS2*}'DNSS2'
-feature_dir=$GLOBAL_PATH/datasets/newfeature/features_win1_with_atch_hmmEm_hmmTr_hhblitsMSA_no_aa
-output_dir=$GLOBAL_PATH/output/model_train_CNNSS_win1
-acclog_dir=$GLOBAL_PATH/output/evaluate/paper_review
 
-# python $GLOBAL_PATH/models/Deep1Dconv_ss/scripts/train_deepcov_ss.py  15  40 5 nadam '6'  100 3  $feature_dir $output_dir 25
-python $GLOBAL_PATH/lib/test_dnss.py $GLOBAL_PATH/datasets/dnss2_train.lst  15 40 5 nadam '6' $feature_dir $output_dir $acclog_dir 'deepss_1dconv' 'train' 25
-python $GLOBAL_PATH/lib/test_dnss.py $GLOBAL_PATH/datasets/dnss2_val.lst  15 40 5 nadam '6' $feature_dir $output_dir $acclog_dir 'deepss_1dconv' 'evalu' 25
-python $GLOBAL_PATH/lib/test_dnss.py $GLOBAL_PATH/datasets/dnss2-blind-test.lst  15 40 5 nadam '6' $feature_dir $output_dir $acclog_dir 'deepss_1dconv' 'test' 25
+if [ -f "$GLOBAL_PATH/python_virtualenv_DNSS2/bin/activate" ]; then
+    echo "Found virutal environment $GLOBAL_PATH/python_virtualenv_DNSS2/bin/activate."
+else 
+    echo "Virtual environment ($GLOBAL_PATH/python_virtualenv_DNSS2/bin/activate) does not exist."
+    exit
+fi
+
+source $GLOBAL_PATH/python_virtualenv_DNSS2/bin/activate
+echo "Training 3-class secondary structure"
+#module load R/R-3.3.1
+
+feature_dir=$GLOBAL_PATH/train_DNSS2/datasets/features_win1_with_atch_hmmEm_hmmTr_hhblitsMSA_no_aa_3class
+output_dir=$GLOBAL_PATH/output/model_train_CNNSS_win1_3class
+acclog_dir=$GLOBAL_PATH/output/evaluate/
+
+python $GLOBAL_PATH/train_DNSS2/models/Deep1Dconv_ss/scripts/train_deepcov_ss_3class.py  15  40 5 nadam '6'  100 3  $feature_dir $output_dir 25
+#python $GLOBAL_PATH/lib/test_dnss.py $GLOBAL_PATH/datasets/dnss2_train.lst  15 40 5 nadam '6' $feature_dir $output_dir $acclog_dir 'deepss_1dconv' 'train' 25
+#python $GLOBAL_PATH/lib/test_dnss.py $GLOBAL_PATH/datasets/dnss2_val.lst  15 40 5 nadam '6' $feature_dir $output_dir $acclog_dir 'deepss_1dconv' 'evalu' 25
+#python $GLOBAL_PATH/lib/test_dnss.py $GLOBAL_PATH/datasets/dnss2-blind-test.lst  15 40 5 nadam '6' $feature_dir $output_dir $acclog_dir 'deepss_1dconv' 'test' 25
